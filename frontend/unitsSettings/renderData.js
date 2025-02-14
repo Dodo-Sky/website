0f19f068c52a07694e8800d45291ea594e94aaef
Zuev Maxim <159797443+zuev5552@users.noreply.github.com>
2025-02-14 21:52:23 +0500

    сообщение любое

--- frontend/unitsSettings/renderData.js
+++ frontend/unitsSettings/renderData.js
@@ -1,43 +1,43 @@
-import { getServerApi } from '../apiServer.js';
-import { getForm } from './workingForm.js';
-import * as components from '../components.js';
+import { getServerApi } from "../apiServer.js";
+import { getForm } from "./workingForm.js";
+import * as components from "../components.js";
 
 export async function renderData() {
-  const unitsSettings = await getServerApi('unitsSettings');
+  const unitsSettings = await getServerApi("unitsSettings");
 
   // отрисовка верхней навигации
-  const unitSettings_nav = document.querySelector('.unitSettings_nav');
+  const unitSettings_nav = document.querySelector(".unitSettings_nav");
 
   const navEl = components.getNavEl();
   const ulEL_nav = components.getUlEl_nav();
 
-  const liEl_dropdown_toggle = components.getLiEl_dropdown_toggle('Пиццерии');
+  const liEl_dropdown_toggle = components.getLiEl_dropdown_toggle("Пиццерии");
   const ulEl_dropdown_menu = components.getUlEl_dropdown_menu();
   liEl_dropdown_toggle.append(ulEl_dropdown_menu);
   for (const unit of unitsSettings) {
-    if (unit.type !== 'Пиццерия') continue;
+    if (unit.type !== "Пиццерия") continue;
     const liEl_dropdown_item = components.getLiEl_dropdown_item(unit.unitName);
     ulEl_dropdown_menu.append(liEl_dropdown_item);
   }
-  const liEl_prz = components.getLiEl_nav('Тюмень-ПРЦ-3');
-  const liEl_ofis = components.getLiEl_nav('Офис');
+  const liEl_prz = components.getLiEl_nav("Тюмень-ПРЦ-3");
+  const liEl_ofis = components.getLiEl_nav("Офис");
   ulEL_nav.append(liEl_dropdown_toggle, liEl_prz, liEl_ofis);
   navEl.append(ulEL_nav);
   unitSettings_nav.append(navEl);
 
   // активация кнопок
-  unitSettings_nav.addEventListener('click', function (e) {
-    let liEl = unitSettings_nav.querySelectorAll('.nav-item');
+  unitSettings_nav.addEventListener("click", function (e) {
+    let liEl = unitSettings_nav.querySelectorAll(".nav-item");
     liEl.forEach((el) => {
-      el.classList.remove('active');
+      el.classList.remove("active");
     });
-    if (e.target.className.includes('nav-item nav-link')) {
-      e.target.classList.add('active');
+    if (e.target.className.includes("nav-item nav-link")) {
+      e.target.classList.add("active");
     }
 
     // отрисовка пиццерий
-    const $content = document.querySelector('.unitSettings_content');
-    $content.innerHTML = '';
+    const $content = document.querySelector(".unitSettings_content");
+    $content.innerHTML = "";
     for (const unit of unitsSettings) {
       if (unit.unitName !== e.target.textContent) continue;
 
@@ -45,33 +45,29 @@ export async function renderData() {
       const unitNameEl = components.getPEl(`Название - ${unit.unitName}`);
       $content.append(title, unitNameEl);
 
-      if (unit.type === 'Пиццерия') {
-        const deliveryWork = components.getPEl(
-          `Время работы доставки: с ${unit.timeWork.delivery.workingTimeStart} до ${unit.timeWork.delivery.workingTimeStop}`,
-        );
-        deliveryWork.classList.add('mb-0');
-        const restoranWork = components.getPEl(
-          `Время работы ресторана: с ${unit.timeWork.restoran.workingTimeStart} до ${unit.timeWork.restoran.workingTimeStop}`,
-        );
+      if (unit.type === "Пиццерия") {
+        const deliveryWork = components.getPEl(`Время работы доставки: с ${unit.timeWork.delivery.workingTimeStart} до ${unit.timeWork.delivery.workingTimeStop}`);
+        deliveryWork.classList.add("mb-0");
+        const restoranWork = components.getPEl(`Время работы ресторана: с ${unit.timeWork.restoran.workingTimeStart} до ${unit.timeWork.restoran.workingTimeStop}`);
 
         // таблица по программам
         const table = components.getTableEl();
-        const captionEl1 = components.getCaptionEl('Список программ');
+        const captionEl1 = components.getCaptionEl("Список программ");
         const tHead = components.getTheadEl();
         const tBody = components.getTbodyEl();
         const trEl = components.getTrEl();
-        const thName = components.getThEl('Наименование программы');
-        const thStatus = components.getThEl('Состояние');
+        const thName = components.getThEl("Наименование программы");
+        const thStatus = components.getThEl("Состояние");
         trEl.append(thName, thStatus);
         tHead.append(trEl);
         for (const program of unit.programs) {
           const trEl = components.getTrEl();
           let tdElname = components.getTdEl(program.name);
 
-          let isActive = program.isActive ? 'Включена' : 'Отключена';
+          let isActive = program.isActive ? "Включена" : "Отключена";
           let tdElactive = components.getTdEl(isActive);
-          if (isActive === 'Включена') tdElactive.classList.add('table-success');
-          if (isActive === 'Отключена') tdElactive.classList.add('table-danger');
+          if (isActive === "Включена") tdElactive.classList.add("table-success");
+          if (isActive === "Отключена") tdElactive.classList.add("table-danger");
           trEl.append(tdElname, tdElactive);
           tBody.append(trEl);
         }
@@ -79,22 +75,20 @@ export async function renderData() {
         $content.append(deliveryWork, restoranWork, table);
       }
 
-      if (unit.type === 'Тюмень-ПРЦ-3') {
-        const deliveryWork = components.getPEl(
-          `Время работы ПРЦ: с ${unit.timeWork.workingTimeStart} до ${unit.timeWork.workingTimeStop}`,
-        );
+      if (unit.type === "Тюмень-ПРЦ-3") {
+        const deliveryWork = components.getPEl(`Время работы ПРЦ: с ${unit.timeWork.workingTimeStart} до ${unit.timeWork.workingTimeStop}`);
         $content.append(deliveryWork);
       }
 
       // таблица по ID телеграмм
       const tableId = components.getTableEl();
-      const captionEl = components.getCaptionEl('Список ID телеграмм');
+      const captionEl = components.getCaptionEl("Список ID телеграмм");
       const tHeadId = components.getTheadEl();
       const tBodyId = components.getTbodyEl();
       const trElId = components.getTrEl();
-      const thId = components.getThEl('Id телеграмм');
-      const thNameFunction = components.getThEl('Функция');
-      const thfio = components.getThEl('ФИО');
+      const thId = components.getThEl("Id телеграмм");
+      const thNameFunction = components.getThEl("Функция");
+      const thfio = components.getThEl("ФИО");
       trElId.append(thId, thNameFunction, thfio);
       tHeadId.append(trElId);
 
@@ -109,8 +103,8 @@ export async function renderData() {
       tableId.append(captionEl, tHeadId, tBodyId);
       $content.append(tableId);
 
-      let btnEdit = components.getButtonEl('Редактировать подразделение', 'submit');
-      btnEdit.setAttribute('data-id', unit.unitId);
+      let btnEdit = components.getButtonEl("Редактировать подразделение", "submit");
+      btnEdit.setAttribute("data-id", unit.unitId);
       $content.append(btnEdit);
     }
   });
--- frontend/unitsSettings/workingForm.js
+++ frontend/unitsSettings/workingForm.js
@@ -1,40 +1,39 @@
-import { getServerApi } from '../apiServer.js';
-import * as components from '../components.js';
+import { getServerApi } from "../apiServer.js";
+import * as components from "../components.js";
 
 export async function getForm() {
-  const unitsSettings = await getServerApi('unitsSettings');
-  const $unitSettings_content = document.querySelector('.unitSettings_content');
-  $unitSettings_content.addEventListener('click', function (e) {
-    if (e.target.textContent === 'Редактировать подразделение') {
-      $unitSettings_content.innerHTML = '';
+  const unitsSettings = await getServerApi("unitsSettings");
+  const $unitSettings_content = document.querySelector(".unitSettings_content");
+  $unitSettings_content.addEventListener("click", function (e) {
+    if (e.target.textContent === "Редактировать подразделение") {
+      $unitSettings_content.innerHTML = "";
       const unit = unitsSettings.find((el) => el.unitId === e.target.dataset.id);
-      console.log(unit);
 
       // Редактировать ID в телеграмм
-      const formEl = components.getFormEl('form');
-      const divRowEl = components.getDivEl('mb-3');
-      let hEl = components.getHEl(5, 'Контактные данные');
-      divRowEl.classList.add('row');
+      const formEl = components.getFormEl("form");
+      const divRowEl = components.getDivEl("mb-3");
+      let hEl = components.getHEl(5, "Контактные данные");
+      divRowEl.classList.add("row");
       formEl.append(divRowEl);
-      const divElId = components.getDivEl('col-md-3');
-      divElId.textContent = 'ID в телеграмм';
-      const divElPosizion = components.getDivEl('col-md-4');
-      divElPosizion.textContent = 'Функция';
-      const divElfio = components.getDivEl('col-md-5');
-      divElfio.textContent = 'ФИО';
+      const divElId = components.getDivEl("col-md-3");
+      divElId.textContent = "ID в телеграмм";
+      const divElPosizion = components.getDivEl("col-md-4");
+      divElPosizion.textContent = "Функция";
+      const divElfio = components.getDivEl("col-md-5");
+      divElfio.textContent = "ФИО";
 
       unit.idTelegramm.forEach((el) => {
-        let inputEl = components.getInputEl('', 'text', el.nameFunction);
-        inputEl.classList.add('mb-1');
+        let inputEl = components.getInputEl("", "text", el.nameFunction);
+        inputEl.classList.add("mb-1");
         inputEl.disabled = true;
         divElPosizion.append(inputEl);
 
-        inputEl = components.getInputEl('', 'number', el.id);
-        inputEl.classList.add('mb-1');
+        inputEl = components.getInputEl("", "number", el.id);
+        inputEl.classList.add("mb-1");
         divElId.append(inputEl);
 
-        inputEl = components.getInputEl('', 'text', el.fio);
-        inputEl.classList.add('mb-1');
+        inputEl = components.getInputEl("", "text", el.fio);
+        inputEl.classList.add("mb-1");
         if (!el.fio) inputEl.disabled = true;
         divElfio.append(inputEl);
       });
@@ -43,15 +42,15 @@ export async function getForm() {
       divRowEl.append(hEl, divElPosizion, divElId, divElfio);
 
       // программы
-      if (unit.type === 'Пиццерия') {
-        const divEl = components.getDivEl('mb-3');
+      if (unit.type === "Пиццерия") {
+        const divEl = components.getDivEl("mb-3");
         formEl.append(divEl);
-        hEl = components.getHEl(5, 'Список программ');
+        hEl = components.getHEl(5, "Список программ");
         divEl.append(hEl);
         unit.programs.forEach((el) => {
-          const divElfprograms = components.getDivEl('mb-0');
-          divElfprograms.classList.add('form-check');
-          divElfprograms.classList.add('form-switch');
+          const divElfprograms = components.getDivEl("mb-0");
+          divElfprograms.classList.add("form-check");
+          divElfprograms.classList.add("form-switch");
 
           let inputCheck = components.getInputCheckboxEl(el.name);
           if (el.isActive) inputCheck.checked = true;
@@ -62,64 +61,44 @@ export async function getForm() {
       }
 
       // Рассписания
-      if (unit.type === 'Пиццерия') {
-        let divEl = components.getDivEl('mb-4');
+      if (unit.type === "Пиццерия") {
+        let divEl = components.getDivEl("mb-4");
         formEl.append(divEl);
-        hEl = components.getHEl(5, 'Время работы');
+        hEl = components.getHEl(5, "Время работы");
         divEl.append(hEl);
 
         function timeWorkDelivery() {
-          const divElDel = components.getDivEl('row');
-          let pEl = components.getPEl('Время работы пиццерии на доставку');
-          pEl.classList.add('mb-0');
+          const divElDel = components.getDivEl("row");
+          let pEl = components.getPEl("Время работы пиццерии на доставку");
+          pEl.classList.add("mb-0");
           divElDel.append(pEl);
           divEl.append(divElDel);
 
-          let divElStart = components.getDivEl('col-auto');
-          divElStart.classList.add ('mb-3');
-          const startInput = components.getInputEl(
-            '',
-            'time',
-            unit.timeWork.delivery.workingTimeStart,
-            'старт',
-          );
+          let divElStart = components.getDivEl("col-auto");
+          divElStart.classList.add("mb-3");
+          const startInput = components.getInputEl("", "time", unit.timeWork.delivery.workingTimeStart, "старт");
           divElStart.append(startInput);
 
-          let divElStop = components.getDivEl('col-auto');
-          divElStop.classList.add ('mb-3');
-          const stopInput = components.getInputEl(
-            '',
-            'time',
-            unit.timeWork.delivery.workingTimeStop,
-            'стоп',
-          );
+          let divElStop = components.getDivEl("col-auto");
+          divElStop.classList.add("mb-3");
+          const stopInput = components.getInputEl("", "time", unit.timeWork.delivery.workingTimeStop, "стоп");
           divElStop.append(stopInput);
           divElDel.append(divElStart, divElStop);
         }
 
         function timeWorkRestoran() {
-          const divElDel = components.getDivEl('row');
-          let pEl = components.getPEl('Время работы ресторана');
-          pEl.classList.add('mb-0');
+          const divElDel = components.getDivEl("row");
+          let pEl = components.getPEl("Время работы ресторана");
+          pEl.classList.add("mb-0");
           divElDel.append(pEl);
           divEl.append(divElDel);
 
-          let divElStart = components.getDivEl('col-auto');
-          const startInput = components.getInputEl(
-            '',
-            'time',
-            unit.timeWork.restoran.workingTimeStart,
-            'старт',
-          );
+          let divElStart = components.getDivEl("col-auto");
+          const startInput = components.getInputEl("", "time", unit.timeWork.restoran.workingTimeStart, "старт");
           divElStart.append(startInput);
 
-          let divElStop = components.getDivEl('col-auto');
-          const stopInput = components.getInputEl(
-            '',
-            'time',
-            unit.timeWork.restoran.workingTimeStop,
-            'стоп',
-          );
+          let divElStop = components.getDivEl("col-auto");
+          const stopInput = components.getInputEl("", "time", unit.timeWork.restoran.workingTimeStop, "стоп");
           divElStop.append(stopInput);
           divElDel.append(divElStart, divElStop);
         }
@@ -127,7 +106,7 @@ export async function getForm() {
         timeWorkRestoran();
       }
 
-      let btn = components.getButtonEl('внести изменения', 'submit');
+      let btn = components.getButtonEl("внести изменения", "submit");
       $unitSettings_content.append(btn);
     }
   });
