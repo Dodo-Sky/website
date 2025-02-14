f213e2e700ca6cd80ef522c03654e64e95de5d82
Eldos <eldos.baktybekov@gmail.com>
2025-02-14 22:43:43 +0600

    :recycle: Переименовал названия функций-компонентов

--- frontend/components.js
+++ frontend/components.js
@@ -1,38 +1,39 @@
 export {
-  getDivEl,
-  getButtonEl,
-  getHEl,
-  getPEl,
-  getNavEl,
-  getUlEl_nav,
-  getUlEl_dropdown_menu,
-  getLiEl_nav,
-  getLiEl_dropdown_toggle,
-  getLiEl_dropdown_item,
-  getFormEl,
-  getLabelEl,
-  getLabelCheckboxEl,
-  getInputEl,
-  getInputCheckboxEl,
-  getSelectEl,
-  getOptionEl,
-  getDivTableEl,
-  getTableEl,
-  getCaptionEl,
-  getTheadEl,
-  getTbodyEl,
-  getTrEl,
-  getThEl,
-  getTdEl,
+  getTagDiv,
+  getTagButton,
+  getTagHeading,
+  getTagParagraph,
+  getTagNavigation,
+  getTagUnorderedListNav,
+  getTagUnorderedListDropdownMenu,
+  getTagListItemNav,
+  getTagListItemDropdownToggle,
+  getTagListItemDropdownItem,
+  getTagForm,
+  getTagLabel,
+  getTagCheckboxLabel,
+  getTagInput,
+  getTagInputCheckbox,
+  getTagSelect,
+  getTagOption,
+  getTagTableDiv,
+  getTagTable,
+  getTagCaption,
+  getTagTableHead,
+  getTagTableBody,
+  getTagTableRow,
+  getTagTableHeaderCell,
+  etTagTableDataCell,
 };
 
 // Базовая разметка
-function getDivEl(addClass) {
+function getTagDiv(addClass) {
   const element = document.createElement('div');
   element.classList.add(addClass);
   return element;
 }
-function getButtonEl(textContent, type = 'button') {
+
+function getTagButton(textContent, type = 'button') {
   const element = document.createElement('button');
   element.setAttribute('type', type);
   element.classList.add(`btn`);
@@ -40,41 +41,47 @@ function getButtonEl(textContent, type = 'button') {
   element.textContent = textContent;
   return element;
 }
-function getHEl(size, textContent) {
+
+function getTagHeading(size, textContent) {
   const element = document.createElement(`h${size}`);
   element.textContent = textContent;
   return element;
 }
-function getPEl(textContent) {
+
+function getTagParagraph(textContent) {
   const element = document.createElement('p');
   element.textContent = textContent;
   return element;
 }
 
 // Навигация
-function getNavEl() {
+function getTagNavigation() {
   const element = document.createElement('nav');
   return element;
 }
-function getUlEl_nav() {
+
+function getTagUnorderedListNav() {
   const element = document.createElement('ul');
   element.classList.add('nav');
   element.classList.add('nav-tabs');
   return element;
 }
-function getUlEl_dropdown_menu() {
+
+function getTagUnorderedListDropdownMenu() {
   const element = document.createElement('ul');
   element.classList.add('dropdown-menu');
   return element;
 }
-function getLiEl_nav(textContent) {
+
+function getTagListItemNav(textContent) {
   const element = document.createElement('li');
   element.classList.add('nav-item');
   element.classList.add('nav-link');
   element.textContent = textContent;
   return element;
 }
-function getLiEl_dropdown_toggle(textContent) {
+
+function getTagListItemDropdownToggle(textContent) {
   const element = document.createElement('li');
   element.classList.add('nav-item');
   element.classList.add('nav-link');
@@ -83,7 +90,8 @@ function getLiEl_dropdown_toggle(textContent) {
   element.textContent = textContent;
   return element;
 }
-function getLiEl_dropdown_item(textContent) {
+
+function getTagListItemDropdownItem(textContent) {
   const element = document.createElement('li');
   element.classList.add('dropdown-item');
   element.textContent = textContent;
@@ -91,26 +99,29 @@ function getLiEl_dropdown_item(textContent) {
 }
 
 // Элементы форм
-function getFormEl(idForm) {
+function getTagForm(idForm) {
   const element = document.createElement('form');
   element.setAttribute('id', idForm);
   return element;
 }
-function getLabelEl(idInput, textContent) {
+
+function getTagLabel(idInput, textContent) {
   const element = document.createElement('label');
   element.classList.add('form-label');
   element.setAttribute('for', idInput);
   element.textContent = textContent;
   return element;
 }
-function getLabelCheckboxEl(idInput, textContent) {
+
+function getTagCheckboxLabel(idInput, textContent) {
   const element = document.createElement('label');
   element.classList.add('form-check-label');
   element.setAttribute('for', idInput);
   element.textContent = textContent;
   return element;
 }
-function getInputEl(idInput, type = 'text', value = '', placeholder = '') {
+
+function getTagInput(idInput, type = 'text', value = '', placeholder = '') {
   const element = document.createElement('input');
   element.classList.add('form-control');
   element.setAttribute('id', idInput);
@@ -119,19 +130,22 @@ function getInputEl(idInput, type = 'text', value = '', placeholder = '') {
   element.setAttribute('placeholder', placeholder);
   return element;
 }
-function getInputCheckboxEl(idInput) {
+
+function getTagInputCheckbox(idInput) {
   const element = document.createElement('input');
   element.classList.add('form-check-input');
   element.setAttribute('id', idInput);
   element.setAttribute('type', 'checkbox');
   return element;
 }
-function getSelectEl() {
+
+function getTagSelect() {
   const element = document.createElement('select');
   element.classList.add('form-select');
   return element;
 }
-function getOptionEl(textContent, value) {
+
+function getTagOption(textContent, value) {
   const element = document.createElement('option');
   element.setAttribute('value', value);
   element.textContent = textContent;
@@ -139,13 +153,14 @@ function getOptionEl(textContent, value) {
 }
 
 // элементы таблиц
-function getDivTableEl() {
+function getTagTableDiv() {
   // горизонтальная прокрутка
   const element = document.createElement('div');
   element.classList.add('table-responsive');
   return element;
 }
-function getTableEl() {
+
+function getTagTable() {
   const element = document.createElement('table');
   element.classList.add('table');
   element.classList.add('table-hover');
@@ -153,32 +168,38 @@ function getTableEl() {
   element.classList.add('caption-top');
   return element;
 }
-function getCaptionEl(textContent) {
+
+function getTagCaption(textContent) {
   const element = document.createElement('caption');
   element.textContent = textContent;
   return element;
 }
-function getTheadEl() {
+
+function getTagTableHead() {
   const element = document.createElement('thead');
   element.classList.add('table-secondary');
   return element;
 }
-function getTbodyEl() {
+
+function getTagTableBody() {
   const element = document.createElement('tbody');
   return element;
 }
-function getTrEl() {
+
+function getTagTableRow() {
   const element = document.createElement('tr');
   return element;
 }
-function getThEl(textContent) {
+
+function getTagTableHeaderCell(textContent) {
   const element = document.createElement('th');
   element.setAttribute('scope', 'col');
   element.classList.add('fs-6');
   element.textContent = textContent;
   return element;
 }
-function getTdEl(textContent) {
+
+function etTagTableDataCell(textContent) {
   const element = document.createElement('td');
   element.classList.add('fs-6');
   element.textContent = textContent;
--- frontend/unitsSettings/renderData.js
+++ frontend/unitsSettings/renderData.js
@@ -8,19 +8,19 @@ export async function renderData() {
   // отрисовка верхней навигации
   const unitSettings_nav = document.querySelector('.unitSettings_nav');
 
-  const navEl = components.getNavEl();
-  const ulEL_nav = components.getUlEl_nav();
+  const navEl = components.getTagNavigation();
+  const ulEL_nav = components.getTagUnorderedListNav();
 
-  const liEl_dropdown_toggle = components.getLiEl_dropdown_toggle('Пиццерии');
-  const ulEl_dropdown_menu = components.getUlEl_dropdown_menu();
+  const liEl_dropdown_toggle = components.getTagListItemDropdownToggle('Пиццерии');
+  const ulEl_dropdown_menu = components.getTagUnorderedListDropdownMenu();
   liEl_dropdown_toggle.append(ulEl_dropdown_menu);
   for (const unit of unitsSettings) {
     if (unit.type !== 'Пиццерия') continue;
-    const liEl_dropdown_item = components.getLiEl_dropdown_item(unit.unitName);
+    const liEl_dropdown_item = components.getTagListItemDropdownItem(unit.unitName);
     ulEl_dropdown_menu.append(liEl_dropdown_item);
   }
-  const liEl_prz = components.getLiEl_nav('Тюмень-ПРЦ-3');
-  const liEl_ofis = components.getLiEl_nav('Офис');
+  const liEl_prz = components.getTagListItemNav('Тюмень-ПРЦ-3');
+  const liEl_ofis = components.getTagListItemNav('Офис');
   ulEL_nav.append(liEl_dropdown_toggle, liEl_prz, liEl_ofis);
   navEl.append(ulEL_nav);
   unitSettings_nav.append(navEl);
@@ -41,35 +41,35 @@ export async function renderData() {
     for (const unit of unitsSettings) {
       if (unit.unitName !== e.target.textContent) continue;
 
-      const title = components.getHEl(6, `Информация по подразделению ${unit.unitName}`);
-      const unitNameEl = components.getPEl(`Название - ${unit.unitName}`);
+      const title = components.getTagHeading(6, `Информация по подразделению ${unit.unitName}`);
+      const unitNameEl = components.getTagParagraph(`Название - ${unit.unitName}`);
       $content.append(title, unitNameEl);
 
       if (unit.type === 'Пиццерия') {
-        const deliveryWork = components.getPEl(
+        const deliveryWork = components.getTagParagraph(
           `Время работы доставки: с ${unit.timeWork.delivery.workingTimeStart} до ${unit.timeWork.delivery.workingTimeStop}`,
         );
         deliveryWork.classList.add('mb-0');
-        const restoranWork = components.getPEl(
+        const restoranWork = components.getTagParagraph(
           `Время работы ресторана: с ${unit.timeWork.restoran.workingTimeStart} до ${unit.timeWork.restoran.workingTimeStop}`,
         );
 
         // таблица по программам
-        const table = components.getTableEl();
-        const captionEl1 = components.getCaptionEl('Список программ');
-        const tHead = components.getTheadEl();
-        const tBody = components.getTbodyEl();
-        const trEl = components.getTrEl();
-        const thName = components.getThEl('Наименование программы');
-        const thStatus = components.getThEl('Состояние');
+        const table = components.getTagTable();
+        const captionEl1 = components.getTagCaption('Список программ');
+        const tHead = components.getTagTableHead();
+        const tBody = components.getTagTableBody();
+        const trEl = components.getTagTableRow();
+        const thName = components.getTagTableHeaderCell('Наименование программы');
+        const thStatus = components.getTagTableHeaderCell('Состояние');
         trEl.append(thName, thStatus);
         tHead.append(trEl);
         for (const program of unit.programs) {
-          const trEl = components.getTrEl();
-          let tdElname = components.getTdEl(program.name);
+          const trEl = components.getTagTableRow();
+          let tdElname = components.etTagTableDataCell(program.name);
 
           let isActive = program.isActive ? 'Включена' : 'Отключена';
-          let tdElactive = components.getTdEl(isActive);
+          let tdElactive = components.etTagTableDataCell(isActive);
           if (isActive === 'Включена') tdElactive.classList.add('table-success');
           if (isActive === 'Отключена') tdElactive.classList.add('table-danger');
           trEl.append(tdElname, tdElactive);
@@ -80,36 +80,36 @@ export async function renderData() {
       }
 
       if (unit.type === 'Тюмень-ПРЦ-3') {
-        const deliveryWork = components.getPEl(
+        const deliveryWork = components.getTagParagraph(
           `Время работы ПРЦ: с ${unit.timeWork.workingTimeStart} до ${unit.timeWork.workingTimeStop}`,
         );
         $content.append(deliveryWork);
       }
 
       // таблица по ID телеграмм
-      const tableId = components.getTableEl();
-      const captionEl = components.getCaptionEl('Список ID телеграмм');
-      const tHeadId = components.getTheadEl();
-      const tBodyId = components.getTbodyEl();
-      const trElId = components.getTrEl();
-      const thId = components.getThEl('Id телеграмм');
-      const thNameFunction = components.getThEl('Функция');
-      const thfio = components.getThEl('ФИО');
+      const tableId = components.getTagTable();
+      const captionEl = components.getTagCaption('Список ID телеграмм');
+      const tHeadId = components.getTagTableHead();
+      const tBodyId = components.getTagTableBody();
+      const trElId = components.getTagTableRow();
+      const thId = components.getTagTableHeaderCell('Id телеграмм');
+      const thNameFunction = components.getTagTableHeaderCell('Функция');
+      const thfio = components.getTagTableHeaderCell('ФИО');
       trElId.append(thId, thNameFunction, thfio);
       tHeadId.append(trElId);
 
       for (const idTelegram of unit.idTelegramm) {
-        const trEl = components.getTrEl();
-        let tdid = components.getTdEl(idTelegram.id);
-        let tdnameFunction = components.getTdEl(idTelegram.nameFunction);
-        let tdfio = components.getTdEl(idTelegram.fio);
+        const trEl = components.getTagTableRow();
+        let tdid = components.etTagTableDataCell(idTelegram.id);
+        let tdnameFunction = components.etTagTableDataCell(idTelegram.nameFunction);
+        let tdfio = components.etTagTableDataCell(idTelegram.fio);
         trEl.append(tdid, tdnameFunction, tdfio);
         tBodyId.append(trEl);
       }
       tableId.append(captionEl, tHeadId, tBodyId);
       $content.append(tableId);
 
-      let btnEdit = components.getButtonEl('Редактировать подразделение', 'submit');
+      let btnEdit = components.getTagButton('Редактировать подразделение', 'submit');
       btnEdit.setAttribute('data-id', unit.unitId);
       $content.append(btnEdit);
     }
