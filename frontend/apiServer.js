aece0b6dd463c393c42f0f5d82ee525c14fd39b8
Eldos <eldos.baktybekov@gmail.com>
2025-03-01 12:11:12 +0600

    Изменен URL для получения данных с сервера

--- frontend/apiServer.js
+++ frontend/apiServer.js
@@ -64,7 +64,7 @@ export async function getServerApi(variableName) {
 // Загрузка данных с сервера (указываем напрямую наименование переменной выгружаемой с сервера)
 export async function getDataServer(variableName) {
   try {
-    const response = await fetch(`${URL}/globalGet?payload=${variableName}`, {
+    const response = await fetch(`${URL}/${variableName}`, {
       headers: getAuthHeaders(),
     });
     if (!response.ok) {
