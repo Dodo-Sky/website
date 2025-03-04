03cc1a8cdd4692bb6fad7add03c48e637628afc6
Eldos <eldos.baktybekov@gmail.com>
2025-03-04 10:00:13 +0600

    Update API server URL and enhance authentication data storage

--- frontend/apiServer.js
+++ frontend/apiServer.js
@@ -3,9 +3,9 @@
 // с внешней ссылки
 //const URL = 'http://178.46.153.198:1860/api';
 // const URL = 'http://localhost:1880';
-
+const URL = 'https://dodo.aitiko.ru/api'
 // по локальной сети
-const URL = 'http://190.186.72.106:86/api';
+// const URL = 'http://190.186.72.106:86/api';
 
 export function getAuthHeaders() {
   const token = localStorage.getItem('token');
@@ -28,6 +28,8 @@ export async function loginServerApi(login, password, onSuccess) {
   if (data.token) {
     localStorage.setItem('token', data.token);
     localStorage.setItem('role', data.role);
+    localStorage.setItem('fio', data.fio)
+    localStorage.setItem('unitName', data.unitName)
     onSuccess?.();
   }
 }
