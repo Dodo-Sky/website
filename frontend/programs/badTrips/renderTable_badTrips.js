31af86febdac1cd9ee9fd0c57d2357515cfc43ad
Zuev Maxim <159797443+zuev5552@users.noreply.github.com>
2025-03-05 23:19:58 +0500

    Фильтрация по курьеру

--- frontend/programs/badTrips/renderTable_badTrips.js
+++ frontend/programs/badTrips/renderTable_badTrips.js
@@ -5,7 +5,7 @@ const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]
 const tooltipList = [...tooltipTriggerList].map((tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl));
 
 export async function renderTable(arrayData, time) {
-  arrayData.sort((a, b) => new Date(a.handedOverToDeliveryAt) - new Date(b.handedOverToDeliveryAt));
+  // arrayData.sort((a, b) => new Date(a.handedOverToDeliveryAt) - new Date(b.handedOverToDeliveryAt));
 
   const tableContent = document.querySelector(".badTrips-table");
   tableContent.innerHTML = "";
