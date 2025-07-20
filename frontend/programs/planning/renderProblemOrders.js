ca9cafdab8839c508a404e8215aa76b42ce2cf1e
denkovalsky <artyom.noskov.1998@gmail.com>
2025-07-20 23:22:30 +0500

    Планерки. Проблемные поездки. - Изменение сортировки рейтинга

--- frontend/programs/planning/renderProblemOrders.js
+++ frontend/programs/planning/renderProblemOrders.js
@@ -39,8 +39,8 @@ const buildBody = (arrayData) => {
 
     const topCount = arrayData.length >= 4 ? 2 : 1;
 
-    const allNoProblemRanks = [...new Set(arrayData.map(o => +o.ranc))].sort((a, b) => b - a);
-    const allAvgRanks = [...new Set(arrayData.map(o => +o.ranc_avg_raiting))].sort((a, b) => a - b);
+    const allNoProblemRanks = [...new Set(arrayData.map(o => +o.ranc))].sort((a, b) => a - b);
+    const allAvgRanks = [...new Set(arrayData.map(o => +o.ranc_avg_raiting))].sort((a, b) => b - a);
 
     const worstNoProblemRank = allNoProblemRanks[0];
     const preWorstNoProblemRank = arrayData.length >= 4 ? allNoProblemRanks[1] : null;
