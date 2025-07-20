b546dccb80702d4d20f1c7b0e1e90eea9701500a
Eldos <eldos.baktybekov@gmail.com>
2025-07-20 15:20:27 +0600

    Enhance API calls with authentication headers and update API URL

--- frontend/config.js
+++ frontend/config.js
@@ -1,2 +1,2 @@
-export const URL = process.env.API_URL || 'https://dodo-sky.ru/api';
+export const URL = process.env.API_URL || 'http://87.228.102.120:1880/api';
 export const SUPPORT_URL = process.env.SUPPORT_URL;
