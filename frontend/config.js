082080057f640518e61adb50d80da2785181b844
denkovalsky <artyom.noskov.1998@gmail.com>
2025-07-21 23:02:10 +0500

    Доработка config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,4 @@
-export const URL = process.env.API_URL || 'http://87.228.102.120:1880/api';
+export const URL = process.env.API_URL || 'http://localhost:1880/api';
 export const SUPPORT_URL = process.env.SUPPORT_URL;
 
 localStorage.setItem("URL", URL)
